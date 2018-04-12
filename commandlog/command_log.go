800c87110e01b4491611853d5933ecba0d07e568
Kelly Sommers <kell.sommers@gmail.com>
2018-04-12 01:24:47 -0400

    Adding support for afero file system and basic support for opening the command log file.

--- commandlog/command_log.go
+++ commandlog/command_log.go
@@ -1,8 +1,64 @@
 package commandlog
 
+import (
+	"os"
+
+	"github.com/spf13/afero"
+)
+
+// CommandLog Represents an instance of a command log.
 type CommandLog struct {
+	fs   afero.Fs
+	file afero.File
 }
 
-func New() *CommandLog {
-	return &CommandLog{}
+// NewInMemory returns a new instance of CommandLog with an in memory log file.
+func NewInMemory() *CommandLog {
+	return new(true)
+}
+
+// NewOnDisk returns a new instance of CommandLog with an on disk log file.
+func NewOnDisk() *CommandLog {
+	return new(false)
+}
+
+// new Returns a new instance of CommandLog.
+func new(inmemory bool) *CommandLog {
+	var fs afero.Fs
+	if inmemory {
+		fs = afero.NewMemMapFs()
+	} else {
+		fs = afero.NewOsFs()
+	}
+	return &CommandLog{
+		fs: fs,
+	}
+}
+
+// Open opens the specified log file for reading and writing.
+func (log *CommandLog) Open(path string, perm os.FileMode) error {
+	var err error
+
+	exists, _ := afero.Exists(log.fs, path)
+	if exists {
+		log.file, err = log.fs.OpenFile(path, os.O_RDWR, perm)
+		if err != nil {
+			return err
+		}
+	} else {
+		log.file, err = log.fs.Create(path)
+		if err != nil {
+			return err
+		}
+		err = log.fs.Chmod(path, perm)
+		if err != nil {
+			return err
+		}
+	}
+	return nil
+}
+
+// Close closes the command log file on disk.
+func (log *CommandLog) Close() error {
+	return log.file.Close()
 }
--- commandlog/command_log_test.go
+++ commandlog/command_log_test.go
@@ -7,6 +7,28 @@ import (
 )
 
 func TestNew(t *testing.T) {
-	log := New()
+	log := NewInMemory()
 	assert.NotNil(t, log)
+}
+
+func TestOpen(t *testing.T) {
+	log := NewInMemory()
+	assert.NotNil(t, log)
+
+	err := log.Open("command.log", 0666)
+	assert.NoError(t, err)
+}
+
+func TestOpenCloseReopen(t *testing.T) {
+	log := NewInMemory()
+	assert.NotNil(t, log)
+
+	err := log.Open("command.log", 0666)
+	assert.NoError(t, err)
+
+	err = log.Close()
+	assert.NoError(t, err)
+
+	err = log.Open("command.log", 0666)
+	assert.NoError(t, err)
 }
