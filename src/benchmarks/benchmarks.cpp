b4645b6a89d11bf48467aa18f04146666f164115
Kelly Sommers <kell.sommers@gmail.com>
2019-01-24 18:12:52 -0500

    Refactored catch2 tests to reduce compile time when iterating tests and make the benchmarks more reusable and readable.

--- src/benchmarks/benchmarks.cpp
+++ src/benchmarks/benchmarks.cpp
@@ -1,4 +1,3 @@
-#define CATCH_CONFIG_MAIN
 #include <time.h>
 #include <stdlib.h>
 #include <sys/types.h>
@@ -9,9 +8,10 @@
 #include "lmdb.h"
 #include "rewind.h"
 
-TEST_CASE("LMDB random writes without Rewind", "[rewind]") {
-    const char* path = "/tmp/benchmark1";
+const bool USE_LMDB = false;
+const bool USE_REWIND = true;
 
+void run_benchmark(bool use_rewind, char* path, int number_of_txn, int number_of_puts_per_txn, int size_of_record) {
     struct stat st = {0};
 
     if (stat(path, &st) == -1) {
@@ -27,23 +27,24 @@ TEST_CASE("LMDB random writes without Rewind", "[rewind]") {
     MDB_val key;
     MDB_val value;
 
-    REQUIRE( SuccessFrom( mdb_env_create(&env) ) );
+    if (use_rewind) {
+        REQUIRE(SuccessFrom(rew_env_create(&env)));
+    } else {
+        REQUIRE(SuccessFrom(mdb_env_create(&env)));
+    }
     REQUIRE( SuccessFrom( mdb_env_set_mapsize(env, size_t(2048576000) ) ) );
     REQUIRE( SuccessFrom( mdb_env_open(env, path, 0, 0664) ) );
     REQUIRE( SuccessFrom( mdb_txn_begin(env, nullptr, 0, &txn) ) );
     REQUIRE( SuccessFrom( mdb_open(txn, nullptr, 0, &dbi) ) );
     REQUIRE( SuccessFrom( mdb_txn_commit(txn) ) );
 
-    int batches = 1000;
-    int records_per_patch = 500;
-    int size_of_value = 1000;
-    char* value_buffer = (char*)malloc(size_of_value);
+    char* value_buffer = (char*)calloc(size_of_record, 1);
 
-    for(int i = 0;i<batches;i++) {
+    for(int i = 0;i<number_of_txn;i++) {
         MDB_txn *tx;
         mdb_txn_begin(env, nullptr, 0, &tx);
 
-        for(int y = 0;y<records_per_patch;y++) {
+        for(int y = 0;y<number_of_puts_per_txn;y++) {
             int val = rand()%1024;
             MDB_val key;
             MDB_val value;
@@ -52,7 +53,7 @@ TEST_CASE("LMDB random writes without Rewind", "[rewind]") {
             key.mv_size = sizeof(int);
             key.mv_data = &r;
 
-            value.mv_size = size_of_value;
+            value.mv_size = size_of_record;
             value.mv_data = value_buffer;
 
             REQUIRE( SuccessFrom( mdb_put(tx, dbi, &key, &value, 0) ) );
@@ -62,55 +63,10 @@ TEST_CASE("LMDB random writes without Rewind", "[rewind]") {
     mdb_env_close(env);
 }
 
+TEST_CASE("LMDB random writes without Rewind", "[rewind]") {
+    run_benchmark(USE_LMDB, "/tmp/benchmark1", 1000, 500, 1000);
+}
+
 TEST_CASE("LMDB random writes with Rewind", "[rewind]") {
-    const char* path = "/tmp/benchmark2";
-
-    struct stat st = {0};
-
-    if (stat(path, &st) == -1) {
-        mkdir(path, 0700);
-    }
-
-    srand(256);
-
-    int rc;
-    MDB_env* env;
-    MDB_dbi dbi;
-    MDB_txn *txn;
-    MDB_val key;
-    MDB_val value;
-
-    REQUIRE( SuccessFrom( rew_env_create(&env) ) );
-    REQUIRE( SuccessFrom( mdb_env_set_mapsize(env, size_t(1048576000) ) ) );
-    REQUIRE( SuccessFrom( mdb_env_open(env, path, 0, 0664) ) );
-    REQUIRE( SuccessFrom( mdb_txn_begin(env, nullptr, 0, &txn) ) );
-    REQUIRE( SuccessFrom( mdb_open(txn, nullptr, 0, &dbi) ) );
-    REQUIRE( SuccessFrom( mdb_txn_commit(txn) ) );
-
-    int batches = 1000;
-    int records_per_patch = 500;
-    int size_of_value = 1000;
-    char* value_buffer = (char*)malloc(size_of_value);
-
-    for(int i = 0;i<batches;i++) {
-        MDB_txn *tx;
-        mdb_txn_begin(env, nullptr, 0, &tx);
-
-        for(int y = 0;y<records_per_patch;y++) {
-            int val = rand()%1024;
-            MDB_val key;
-            MDB_val value;
-            int r = rand();
-
-            key.mv_size = sizeof(int);
-            key.mv_data = &r;
-
-            value.mv_size = size_of_value;
-            value.mv_data = value_buffer;
-
-            REQUIRE( SuccessFrom( mdb_put(tx, dbi, &key, &value, 0) ) );
-        }
-        mdb_txn_commit(tx);
-    }
-    mdb_env_close(env);
+    run_benchmark(USE_REWIND, "/tmp/benchmark2", 1000, 500, 1000);
 }
